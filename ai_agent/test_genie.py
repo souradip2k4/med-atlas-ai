fc932ad34129a4650c44f65d6eef5b3a37fcc88f
souradip2k4 <22052939@kiit.ac.in>
2026-03-25 17:46:55 +0530

    Fix vector search logic

--- ai_agent/test_genie.py
+++ ai_agent/test_genie.py
@@ -11,8 +11,8 @@ GENIE_ID = os.environ["GENIE_SPACE_ID"]
 try:
     agent = GenieAgent(genie_space_id=GENIE_ID, return_pandas=False)
     print("Testing string invoke...")
-    res = agent.invoke("How many hospitals?")
-    print("RES:", type(res), res)
+    res = agent.invoke({"messages": [{"role": "user", "content": "How many hospitals?"}]})
+    print("RES:", type(res))
 except Exception as e:
     import traceback
     traceback.print_exc()
