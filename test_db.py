288a23585281d41327bbdc82b6fc118181a7b1a8
souradip2k4 <22052939@kiit.ac.in>
2026-04-29 22:22:44 +0530

    Remove unnecessary files

--- test_db.py
+++
@@ -1,8 +0,0 @@
-from IDP.storage.database import DatabaseManager
-
-db = DatabaseManager()
-try:
-    df = db.read_delta("facility_records").select("facility_id").limit(10)
-    print("RECORDS", df.collect())
-except Exception as e:
-    print(e)
